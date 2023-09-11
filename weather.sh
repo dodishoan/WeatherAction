1b0d210fc8c5bcc5f3d4858ddc796905af29c465
dodishoan <144530736+dodishoan@users.noreply.github.com>
2023-09-11 09:51:55 +0800

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -eux
 
-CITY=${{ secrets.CITY }}
+#CITY=Jiangmen
+CITY: ${{ secrets.CITY }}
 LANGUAGE="zh-CN"
 UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
